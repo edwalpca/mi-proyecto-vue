376626d1503797a851be71f6b0dc3a98bd04c5ed
Mauricio Alpizar <ealpizar@ticosoftweb.com>
2023-02-10 21:56:41 -0600

    Ejercicio de Reactividad en Vue usando metodo COMPUTED

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 
   //importacion del Paquete que me permite la reactividad en el DOM.
-  import {ref} from 'vue'
+ import {ref, computed} from 'vue'
 
   //Aqui lo puedo definir como una constante porqu tiene mejor performance.
  const contador = ref(0);
@@ -13,28 +13,25 @@
   //Metodo de Resetear
  const restaurar = () => contador.value = 0;
 
- //Parte de La Solucion #1
-  const textoRojo  = 'color: red';
-  const textoVerde = 'color: green';
-  const textogris = 'color: grey';
+ // Cuando usamos un metodo computado siempre la funcion estrictamente
+ // debe retornar ALGO
+ // Para este caso particular estaria retornando el nombre de la clase
+ const classCounter = computed(() => {
+   if (contador.value === 0){
+     return 'neutro';
+   }
+   if (contador.value>0){
+     return 'positivo';
+   }else{
+     return 'negativo'
+   }
+ });
 
 </script>
 
 <template>
-
   <h1>Ejercicio de Reactividad de Vue</h1>
-  <template v-if="(contador > 0)">
-    <h2 v-bind:style="textoVerde"> Contador: {{contador}}</h2>
-  </template>
-  <template v-else-if="(contador == 0)">
-    <h2 :style="textogris"> Contador: {{contador}}</h2>
-  </template>
-  <template v-else>
-    <h2 v-bind:style="textoRojo"> Contador: {{contador}}</h2>
-  </template>
-
-  <h2 v-bind:class="( contador > 0 ) ? 'positivo': ( contador == 0 ) ? 'neutro': 'negativo' ">Contador: {{contador}}</h2>
-
+  <h2 v-bind:class="classCounter">Contador: {{contador}}</h2>
   <div>
       <button @click="incrementar">Aumentar Contador</button>
       <button @click="disminuir">Disminuir Contador</button>
@@ -42,9 +39,7 @@
   </div>
 </template>
 
-
 <style>
-
 button{
   margin: 10px;
 }
@@ -59,5 +54,4 @@ button{
   color: grey;
 
 }
-
 </style>
