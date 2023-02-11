006f69d4e04d6053580729f6d56683b9fa4d269c
Mauricio Alpizar <ealpizar@ticosoftweb.com>
2023-02-10 21:43:01 -0600

    Ejercicio de Reactividad en Vue solucion #2 con opcion optimizada o corta mas eficiente

--- src/App.vue
+++ src/App.vue
@@ -7,31 +7,22 @@
  const contador = ref(0);
 
   //Metodo de Incrementar
- const incrementar = () => {
-   contador.value++;
-   console.log(contador);
- };
-
+ const incrementar = () => contador.value++
   //Metodo de disminuir
- const disminuir = () => {
-   contador.value--;
-   console.log(contador);
- }
-
+ const disminuir = () => contador.value--
   //Metodo de Resetear
- const restaurar = () => {
-   contador.value = 0;
-   console.log(contador);
- }
+ const restaurar = () => contador.value = 0;
 
- const textoRojo  = 'color: red';
- const textoVerde = 'color: green';
- const textogris = 'color: grey';
+ //Parte de La Solucion #1
+  const textoRojo  = 'color: red';
+  const textoVerde = 'color: green';
+  const textogris = 'color: grey';
 
 </script>
 
 <template>
-  <h1>Ejercicio de Reactividad de vue</h1>
+
+  <h1>Ejercicio de Reactividad de Vue</h1>
   <template v-if="(contador > 0)">
     <h2 v-bind:style="textoVerde"> Contador: {{contador}}</h2>
   </template>
@@ -42,16 +33,31 @@
     <h2 v-bind:style="textoRojo"> Contador: {{contador}}</h2>
   </template>
 
+  <h2 v-bind:class="( contador > 0 ) ? 'positivo': ( contador == 0 ) ? 'neutro': 'negativo' ">Contador: {{contador}}</h2>
+
   <div>
-  <button @click="incrementar">Aumentar Contador</button>
-  <button @click="disminuir">Disminuir Contador</button>
-  <button @click="restaurar">Resetar Contador</button>
+      <button @click="incrementar">Aumentar Contador</button>
+      <button @click="disminuir">Disminuir Contador</button>
+      <button @click="restaurar">Resetar Contador</button>
   </div>
 </template>
 
 
 <style>
+
 button{
   margin: 10px;
 }
+
+.negativo{
+  color: red;
+}
+.positivo{
+  color: green;
+}
+.neutro{
+  color: grey;
+
+}
+
 </style>
